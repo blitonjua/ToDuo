8296ff55e3dfdd4be53034a47604aa6d4bc13d50
Olimjon <olimjon.nematov@gmail.com>
2020-06-15 15:35:50 -0500

    fixed a sign up issue

--- screens/authscreen.js
+++ screens/authscreen.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {Modal, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 import auth from '@react-native-firebase/auth';
 // import readFromDatabase from '../services/fire';
-import addUser from '../services/fire';
+import {addUser} from '../services/fire';
 
 const AuthScreen = () => {
   //manage state
@@ -101,7 +101,7 @@ const AuthScreen = () => {
           <View style={styles.container}>
             <Text>Enter your age</Text>
             <TextInput
-              keyboardType="number-pad"
+              // keyboardType="number-pad"
               onChangeText={text => setAgeText(text)}
             />
           </View>
