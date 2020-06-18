6f5341a0f3b9707075214e45b10d35ee5998a58e
Andrew Deng <adeng1433@gmail.com>
2020-06-18 16:09:02 -0500

    updated goalScreen

--- screens/main/goalScreen.js
+++ screens/main/goalScreen.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {
   SafeAreaView,
-  Button,
+  Text
 } from 'react-native';
 
 import auth from '@react-native-firebase/auth';
@@ -11,26 +11,7 @@ import {addGoalToUserGoalCollection} from '../../services/createGoal';
 function GoalScreen() {
   return (
     <SafeAreaView>
-      <Button
-        title="add a goal to waiting room"
-        onPress={() => {
-          //pass in goal id, this is where we connect to Andrea
-          matchUser('tempId3');
-        }}
-      />
-
-      <Button
-        title="+"
-        onPress={() => {
-          //change static values for title, description and milestones
-          addGoalToUserGoalCollection(
-            auth().currentUser.uid,
-            'title',
-            'description',
-            ['milestone1', 'milestone2', 'milestone3'],
-          );
-        }}
-      />
+      <Text>Hi</Text>
     </SafeAreaView>
   );
 };
