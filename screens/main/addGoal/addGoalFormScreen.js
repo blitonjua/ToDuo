062bb35ee97dda39b1b06061b9bb5403f01d7daa
Andrew Deng <adeng1433@gmail.com>
2020-06-18 15:45:06 -0500

    added the add-goal buttons back into AddGoalFormScreen

--- screens/main/addGoal/addGoalFormScreen.js
+++ screens/main/addGoal/addGoalFormScreen.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Text,
     SafeAreaView,
     TouchableOpacity,
+    TextInput,
+    View,
+    Button
 } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+import { addGoalToUserGoalCollection } from '../../../services/createGoal';
+ 
 
 function AddGoalFormScreen({ navigation }) {
-
+    //navigates back to Plus
     function goBack() {
         navigation.navigate('Plus');
     }
+    // listOutDatabase();
+    const addGoalHandler = title => {
+        //some stuff
+    };
+
+    const [goal, setGoal] = useState('');
+
+    const onChangeTextHandler = enteredGoal => {
+        //some stuff
+        setGoal(enteredGoal);
+    };
+
 
     return (
         <SafeAreaView>
@@ -18,6 +37,25 @@ function AddGoalFormScreen({ navigation }) {
                     Go Back because you don't know what your goal is lame-o
                 </Text>
             </TouchableOpacity>
+            <TouchableOpacity onPress={() => addGoalHandler()}>
+                <Text>Add Goal</Text>
+            </TouchableOpacity>
+            <View style={{borderWidth:1}}>
+                <TextInput onChangeText={() => onChangeTextHandler()} value={goal} />
+            </View>
+            <Text>{goal}</Text>
+            <Button
+                title="+"
+                onPress={() => {
+                    //change static values for title, description and milestones
+                    addGoalToUserGoalCollection(
+                        auth().currentUser.uid,
+                        'title',
+                        'description',
+                        ['milestone1', 'milestone2', 'milestone3'],
+                    );
+                }}
+            />
         </SafeAreaView>
     );
 }
