2079194d0b1beb20fff432be5ffa4727d1be3b26
Andrew Deng <adeng1433@gmail.com>
2020-06-19 02:50:22 -0500

    created a submission screen for addgoalstack, fixed some methods and tried to give the goal a proper title

--- screens/main/addGoal/addGoalScreen.js
+++ screens/main/addGoal/addGoalScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
     Text,
     SafeAreaView,
@@ -10,48 +10,65 @@ import {
 import auth from '@react-native-firebase/auth';
 
 import { addGoalToUserGoalCollection } from '../../../services/createGoal';
- 
+
 
 function AddGoalScreen({ navigation }) {
     // listOutDatabase();
     const addGoalHandler = title => {
-        //some stuff
+        addGoalToUserGoalCollection(
+            auth().currentUser.uid,
+            title,
+            'description',
+            ['milestone1', 'milestone2', 'milestone3'],
+        );
+        setSubmitted(true);
     };
 
-    const [goal, setGoal] = useState('');
+    //hooks for goal creation
+    const [title, setTitle] = useState('');
+    const [submitted, setSubmitted] = useState(false);
 
-    const onChangeTextHandler = enteredGoal => {
-        //some stuff
-        setGoal(enteredGoal);
-    };
-
+    //sets the state to the goal title
+    function titleHandler(enteredTitle) {
+        setTitle(enteredTitle);
+    }
 
     return (
         <SafeAreaView>
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-                <Text>
-                    Go Back because you don't know what your goal is lame-o
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => addGoalHandler()}>
-                <Text>Add Goal</Text>
-            </TouchableOpacity>
-            <View style={{borderWidth:1}}>
-                <TextInput onChangeText={() => onChangeTextHandler()} value={goal} />
-            </View>
-            <Text>{goal}</Text>
-            <Button
-                title="+"
-                onPress={() => {
-                    //change static values for title, description and milestones
-                    addGoalToUserGoalCollection(
-                        auth().currentUser.uid,
-                        'title',
-                        'description',
-                        ['milestone1', 'milestone2', 'milestone3'],
-                    );
-                }}
-            />
+            {!submitted ? (
+                <View>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Text>
+                            Go Back because you don't know what your goal is lame-o
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => addGoalHandler()}>
+                        <Text>Add Goal</Text>
+                    </TouchableOpacity>
+
+                    <View style={{ borderWidth: 1 }}>
+                        <TextInput onChangeText={titleHandler} value={title} />
+                    </View>
+
+                    <Text>{title}</Text>
+                    <Button
+                        title="+"
+                        onPress={() => addGoalHandler()}
+                    />
+                </View>
+            ) : (
+                <View>
+                    <Text>
+                        Goal Added! This will be some confirmation text.
+                    </Text>
+
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Text>
+                            Go Back
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            )}
         </SafeAreaView>
     );
 }
