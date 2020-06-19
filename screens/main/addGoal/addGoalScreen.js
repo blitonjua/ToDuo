b35eb1186b012fa4180ada9ab300bbff065e36f2
Andrew Deng <adeng1433@gmail.com>
2020-06-19 15:17:28 -0500

    added fields that work to add goals

--- screens/main/addGoal/addGoalScreen.js
+++ screens/main/addGoal/addGoalScreen.js
@@ -13,24 +13,48 @@ import { addGoalToUserGoalCollection } from '../../../services/createGoal';
 
 
 function AddGoalScreen({ navigation }) {
-    // listOutDatabase();
-    const addGoalHandler = title => {
+    //hooks for goal creation
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [milestone1, setMilestone1] = useState('');
+    const [milestone2, setMilestone2] = useState('');
+    const [milestone3, setMilestone3] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    //creates a goal and adds it to the database
+    function addGoalHandler() {
         addGoalToUserGoalCollection(
             auth().currentUser.uid,
             title,
-            'description',
-            ['milestone1', 'milestone2', 'milestone3'],
+            description,
+            [milestone1, milestone2, milestone3],
         );
         setSubmitted(true);
     };
 
-    //hooks for goal creation
-    const [title, setTitle] = useState('');
-    const [submitted, setSubmitted] = useState(false);
-
-    //sets the state to the goal title
+    //sets title
     function titleHandler(enteredTitle) {
         setTitle(enteredTitle);
+    }
+
+    //sets description
+    function descriptionHandler(enteredDescription) {
+        setDescription(enteredTitle);
+    }
+
+    //sets milestone1
+    function milestone1Handler(enteredMilestone) {
+        setMilestone1(enteredMilestone);
+    }
+
+    //sets milestone2
+    function milestone2Handler(enteredMilestone) {
+        setMilestone2(enteredMilestone);
+    }
+
+    //sets milestone3
+    function milestone3Handler(enteredMilestone) {
+        setMilestone3(enteredMilestone);
     }
 
     return (
@@ -42,15 +66,33 @@ function AddGoalScreen({ navigation }) {
                             Go Back because you don't know what your goal is lame-o
                         </Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => addGoalHandler()}>
-                        <Text>Add Goal</Text>
-                    </TouchableOpacity>
 
+                    {/* title */}
                     <View style={{ borderWidth: 1 }}>
-                        <TextInput onChangeText={titleHandler} value={title} />
+                        <TextInput placeholder="title" onChangeText={titleHandler} value={title} />
                     </View>
 
-                    <Text>{title}</Text>
+                    {/* description */}
+                    <View style={{ borderWidth: 1 }}>
+                        <TextInput placeholder="description" onChangeText={descriptionHandler} value={description} />
+                    </View>
+
+                    {/* milestone1 */}
+                    <View style={{ borderWidth: 1 }}>
+                        <TextInput placeholder="milestone1" onChangeText={milestone1Handler} value={milestone1} />
+                    </View>
+
+                    {/* milestone2 */}
+                    <View style={{ borderWidth: 1 }}>
+                        <TextInput placeholder="milestone2" onChangeText={milestone2Handler} value={milestone2} />
+                    </View>
+
+                    {/* milestone3 */}
+                    <View style={{ borderWidth: 1 }}>
+                        <TextInput placeholder="milestone3" onChangeText={milestone3Handler} value={milestone3} />
+                    </View>
+
+                    {/* add goal Button */}
                     <Button
                         title="+"
                         onPress={() => addGoalHandler()}
@@ -60,12 +102,12 @@ function AddGoalScreen({ navigation }) {
                 <View>
                     <Text>
                         Goal Added! This will be some confirmation text.
-                    </Text>
+                </Text>
 
                     <TouchableOpacity onPress={() => navigation.goBack()}>
                         <Text>
                             Go Back
-                        </Text>
+                    </Text>
                     </TouchableOpacity>
                 </View>
             )}
