40e82bffbbb0d104d5a9e70ede2b1cc6ac277abc
Olimjon <olimjon.nematov@gmail.com>
2020-06-21 18:09:16 -0500

    adding the flatlist

--- screens/main/goal/goalsList.js
+++ screens/main/goal/goalsList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   SafeAreaView,
@@ -6,17 +6,74 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import getGoalData from '../../../services/getData';
+import {FlatList} from 'react-native-gesture-handler';
 
 function goalsListScreen({navigation}) {
-  function gotoDashBoard() {
-    navigation.navigate('dashBoard');
+  const [goalData, setGoalData] = useState([]);
+  let uid = auth().currentUser.uid;
+
+  async function getGoals() {
+    let data = await getGoalData(uid);
+    return data;
   }
+
+  const setData = () => {
+    getGoals().then(function(val) {
+      setGoalData(val);
+    });
+  };
+
+  function Item({id, title, selected, onSelect}) {
+    return (
+      <TouchableOpacity
+        onPress={() => onSelect(id)}
+        style={[
+          styles.item,
+          {backgroundColor: selected ? '#6e3b6e' : '#f9c2ff'},
+        ]}>
+        <Text style={styles.title}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+    id => {
+      const newSelected = new Map(selected);
+      newSelected.set(id, !selected.get(id));
+
+      setSelected(newSelected);
+    },
+    [selected],
+  );
+
+  setData();
   return (
     <SafeAreaView style={styles.safe}>
       <View style={styles.main}>
         {/* add the flat list */}
         <Text>This is the goals list</Text>
-        <TouchableOpacity onPress={() => gotoDashBoard()}>
+        <FlatList
+          data={goalData}
+          renderItem={({item}) => (
+            <Item
+              id={item.id}
+              title={item.title}
+              selected={!!selected.get(item.id)}
+              onSelect={onSelect}
+            />
+          )}
+          keyExtractor={item => item.id}
+          extraData={selected}
+        />
+
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('dashBoard');
+          }}>
           <Text>Go to DashBoard</Text>
         </TouchableOpacity>
       </View>
