9f00d2b94e9fae95535faa107d5998da12a3ecd5
Bea Litonjua <bealitonjua@beas-air.lan>
2020-06-25 13:14:08 -0400

    backend todo

--- services/toDoList.js
+++ services/toDoList.js
@@ -1,1 +1,13 @@
-ui;
+import React from 'react';
+import firestore from '@react-native-firebase/firestore';
+
+var db = firestore().collection('Users');
+
+export const addToDo = (uid, goalId, item) => {
+    db.doc(uid).collection('goals').doc(goalId).collection('ToDo').add({
+        itemDescription: item,
+        done: false
+    })
+}
+
+
