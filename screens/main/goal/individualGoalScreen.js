c1a6613041770313db7bd981061d031008090503
Olimjon <olimjon.nematov@gmail.com>
2020-07-14 13:54:22 -0500

    merge msater

--- screens/main/goal/individualGoalScreen.js
+++ screens/main/goal/individualGoalScreen.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-=======
-import React, { useState, useContext } from 'react';
->>>>>>> master
+import React, {useState, useContext} from 'react';
 import {
   SafeAreaView,
   FlatList,
@@ -20,14 +16,10 @@ import {updateStatus} from '../../../services/setGoals';
 //constants
 import {status} from '../../../services/universalConstants';
 //styles
-<<<<<<< HEAD
 import {individualGoalStyles} from '../../../assets/styles/styles';
 import {getMilestonesAsObjects} from '../../../services/getData';
 
-=======
-import { individualGoalStyles } from '../../../assets/styles/styles';
-import { UserContext } from '../../../services/userContext';
->>>>>>> master
+import {UserContext} from '../../../services/userContext';
 const styles = individualGoalStyles;
 
 //the detailed page of a particular goal, displaying milestones, their daily goals, etc.
@@ -35,39 +27,23 @@ function IndividualGoalScreen({route, navigation}) {
   const {goal} = route.params;
   const [toDoList, setToDoList] = useState([]);
   const [toDoText, setToDoText] = useState('');
-<<<<<<< HEAD
   const [milestones, setMilestones] = useState([]);
 
   let uid = auth().currentUser.uid;
 
-  //updates the status of the goal and goes to the done screen
-  function goalDone(status) {
-    updateStatus(goal.goalId, status);
-    navigation.navigate('doneScreen', {status: status});
-=======
   // let user = auth().currentUser.uid;
-  const { user, setUser } = useContext(UserContext)
+  const {user, setUser} = useContext(UserContext);
 
   //updates the status of the goal and goes to the done screen
   function goalDone(status) {
-    updateStatus(user ,goal.goalId, status);
-    navigation.navigate('doneScreen', { status: status })
->>>>>>> master
+    updateStatus(user, goal.goalId, status);
+    navigation.navigate('doneScreen', {status: status});
   }
 
   function gotoMessage() {
     navigation.navigate('messageScreen', {goal: route});
   }
 
-  async function getToDoListData() {
-<<<<<<< HEAD
-    let data = await getToDoList(uid, goal.goalId);
-    setToDoList(data);
-=======
-    let data = await getToDoList(user, goal.goalId);
-    return data;
->>>>>>> master
-  }
   async function getMilestones() {
     let data = await getMilestonesAsObjects(uid, goal.goalId);
     setMilestones(data);
@@ -105,51 +81,6 @@ function IndividualGoalScreen({route, navigation}) {
             keyExtractor={(item, index) => index.toString()}
           />
         </View>
-<<<<<<< HEAD
-=======
-
-        {/* todo list */}
-        <View style={styles.main}>
-          <Text>To Do</Text>
-          <FlatList
-            data={toDoList}
-            renderItem={({ item }) =>
-              //TODO: preferable to move this into a separate function
-              <View style={styles.toDoItem}>
-                <CircleCheckBox
-                  checked={false}
-                  onToggle={() => {
-                    let itemId = item.itemDescription;
-                    deleteItem(user, goal.goalId, itemId);
-                  }}
-                  labelPosition={LABEL_POSITION.RIGHT}
-                  label={item.itemDescription}
-                />
-              </View>
-            }
-            keyExtractor={(item, index) => index.toString()}
-          />
-          <View style={styles.toDoItem}>
-            <TextInput
-              placeholder="Add item to do list"
-              onChangeText={text => setToDoText(text)}
-              ref={input => {
-                this.textInput = input;
-              }}
-            />
-            <Button
-              title="+"
-              onPress={() => {
-                if (toDoText != '') {
-                  addToDo(auth().currentUser.uid, goal.goalId, toDoText);
-                  setToDoText('');
-                  this.textInput.clear();
-                }
-              }}
-            />
-          </View>
-        </View>
->>>>>>> master
       </View>
 
       {/* toDo list button */}
