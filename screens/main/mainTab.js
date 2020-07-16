5d0474d389d773817f420d39e11e9a7b9e5a627a
Jesse Huang <jesse17huang@utexas.edu>
2020-07-16 12:02:09 -0500

    icons added

--- screens/main/mainTab.js
+++ screens/main/mainTab.js
@@ -1,4 +1,4 @@
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React, { useContext } from 'react';
 //firebase
 import auth from '@react-native-firebase/auth';
@@ -7,8 +7,8 @@ import { UserContext } from '../../services/userContext';
 import GoalStack from './goal/goalStack';
 import ProfileStack from './profile/profileStack';
 import AddGoalStack from './addGoal/addGoalStack';
-
-const Tab = createMaterialTopTabNavigator();
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const Tab = createBottomTabNavigator();
 
 function MainTab() {
   const { user, setUser } = useContext(UserContext);
@@ -17,7 +17,29 @@ function MainTab() {
     setUser(cUser.uid)
 
   return (
-    <Tab.Navigator initialRouteName="Goal" tabBarPosition="bottom">
+    <Tab.Navigator initialRouteName="Goal" tabBarPosition="bottom"
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+        } else if (route.name === 'Settings') {
+          iconName = focused ? 'ios-list-box' : 'ios-list';
+        } else if (route.name === 'New'){
+          iconName = focused? 'ios-list-box' : 'ios-list';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }}>
       <Tab.Screen name="Profile" component={ProfileStack} />
       <Tab.Screen name="Goal" component={GoalStack} />
       <Tab.Screen name="New" component={AddGoalStack} />
