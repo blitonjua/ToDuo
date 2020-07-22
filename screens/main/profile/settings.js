90aa0338b365310e7fb79d483ce4feaa5cb45f06
Bea Litonjua <bealitonjua@beas-air.lan>
2020-07-22 19:44:23 -0400

    updated settings screen

--- screens/main/profile/settings.js
+++ screens/main/profile/settings.js
@@ -17,7 +17,8 @@ export async function getUserData(uid) {
           let dataObject = {
               age: docData.age,
               firstName: docData.firstName,
-              lastName: docData.lastName
+              lastName: docData.lastName,
+              email: docData.email
           }
           user = dataObject
         }
@@ -26,29 +27,45 @@ export async function getUserData(uid) {
   return user
   }
 
-  export function updateUserData(uid, firstName, lastName, age) {
+  export function updateFirstName(uid, firstName) {
      db
       .doc(uid)
       .update({
-          age: age,
-          firstName: firstName,
-          lastName: lastName
+          firstName: firstName
       })
-    
-  }
-  
-  const reauthenticate = (currentPassword) => {
-    var currentUser = auth().currentUser;
-    var cred = firebase.auth.EmailAuthProvider.credential(
-        currentUser.email, currentPassword);
-    return currentUser.reauthenticateWithCredential(cred);
   }
 
-  export function changePassword(currentPassword, newPassword) {
-    reauthenticate(currentPassword).then(() => {
-      var currentUser = auth().currentUser;
-      currentUser.updatePassword(newPassword).then(() => {
-        console.log("Password updated!");
-      }).catch((error) => { console.log(error); });
-    }).catch((error) => { console.log(error); });
-  }
+  export function updateLastName(uid, lastName) {
+    db
+     .doc(uid)
+     .update({
+         lastName: lastName
+     })
+ }
+
+ export function updateAge(uid, age) {
+  db
+   .doc(uid)
+   .update({
+       age: age,
+   })
+}
+
+export function forgotPassword(email) {
+  auth().sendPasswordResetEmail(email)
+    .then(function (user) {
+      alert('Please check your email...')
+    }).catch(function (e) {
+      console.log(e)
+    })
+}
+
+  
+export function deleteAccount() {
+  var user = auth().currentUser;
+  user.delete().then(function() {
+    // User deleted.
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
--- screens/main/profile/settingsScreen.js
+++ screens/main/profile/settingsScreen.js
@@ -9,7 +9,10 @@ import {
     Button
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
-import {getUserData, updateUserData, changePassword} from './settings'
+import {getUserData, updateAge, updateFirstName, updateLastName, forgotPassword, deleteAccount} from './settings'
+import {settingsScreenStyles} from '../../../assets/styles/styles'
+
+styles = settingsScreenStyles;
 
 
 function SettingsScreen({ navigation }) {
@@ -17,8 +20,8 @@ function SettingsScreen({ navigation }) {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [age, setAge] = useState('')
-    const [oldPassword, setOldPassword] = useState('')
-    const [newPassword, setNewPassword] = useState('')
+    // const [oldPassword, setOldPassword] = useState('')
+    // const [newPassword, setNewPassword] = useState('')
     const [text, setText] = useState()
 
     const uid = auth().currentUser.uid
@@ -29,55 +32,76 @@ function SettingsScreen({ navigation }) {
         setUserData(user)
       }
 
-    const passwordHandler = (oldPass, newPass) => {
-        changePassword(oldPass, newPass)
-    }
-
     useEffect(() => {
         getUser();
     },[])
 
 
     return(
-        <SafeAreaView>
-            <View>
-                <TextInput value={text} onChangeText={(val) => {
-                    console.log(val)
-
-                    setFirstName(val)}
-                
-                }/>
-                <TextInput value={text} onChangeText={(val) => setLastName(val)}/>
-                <TextInput value = {text} onChangeText={(val) => setAge(val)}/>
-                <TouchableOpacity onPress={() => {
-                    updateUserData(uid, firstName, lastName, age)
-                    setText('')
+        <SafeAreaView style={styles.mainContainer}>
+            <Text style={styles.title}>Profile Settings</Text>
+            <View style={styles.padding}> 
+                <View style={styles.container}>
+                    <TextInput placeholderTextColor="#000" placeholder={userData.firstName} value={text} onChangeText={(val) => {setFirstName(val)} }/>
+                </View>
+                    <TouchableOpacity style={styles.button} onPress={() => {
+                    updateFirstName(uid, firstName)
+                    getUser()
                     }
                     }>
-                    <Text> Change </Text>
+                    <Text> Change First Name </Text>
                 </TouchableOpacity>
-                <TextInput onChangeText={(val) => {
-                 setOldPassword(val) }
-                }/>
-                <TextInput onChangeText={(val) => setNewPassword(val)}/>
-                <Button title="Button"onPress={ () => {
-                    passwordHandler(oldPassword, newPassword)
-
-                }}/>
-                    <Text> Change Password</Text>
+                <View style={styles.container}>
+                    <TextInput placeholderTextColor="#000" placeholder={userData.lastName} value={text} onChangeText={(val) => setLastName(val)}/>
+                </View>
+                    <TouchableOpacity style={styles.button} onPress={() => {
+                    updateLastName(uid, lastName)
+                    getUser()
+                    }
+                    }>
+                    <Text> Change Last Name </Text>
+                </TouchableOpacity>
+                <View style={styles.container}>
+                    <TextInput placeholderTextColor="#000" placeholder={userData.age} value = {text} onChangeText={(val) => setAge(val)}/>
+                </View>
+                    <TouchableOpacity style={styles.button} onPress={() => {
+                    updateAge(uid, age)
+                    getUser()
+                    }
+                    }>
+                    <Text> Change Age </Text>
+                </TouchableOpacity>
+                <View style={styles.resetPassword}>
+                <TouchableOpacity style={styles.button} onPress={() => {
+                    forgotPassword(userData.email)
+                    }
+                    }>
+                    <Text> Reset password </Text>
+                </TouchableOpacity>
+                </View>
+                <View style={styles.resetPassword}>
+                <TouchableOpacity style={styles.button} onPress={() => {
+                    deleteAccount()
+                    }
+                    }>
+                    <Text> Deactivate Account </Text>
+                </TouchableOpacity>
+                </View>
+    
+                
             
             </View>
+            <View style={styles.goBack}>
 
-            <TouchableOpacity onPress={() => navigation.goBack()}>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
                 <Text> Go Back </Text>
             </TouchableOpacity>
+
+            </View>
            
         </SafeAreaView>
     );
 };
 
-// changing password
-// update name
-// delete account
 
 export default SettingsScreen;
